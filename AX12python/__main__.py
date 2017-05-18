b8bf5853a15976b05988ebaf1ae8d44c40707a49
RickSchoffelmeer <rickschoffelmeer@gmail.com>
2017-05-18 15:37:09 +0200

    Message

--- AX12python/__main__.py
+++ AX12python/__main__.py
@@ -3,11 +3,12 @@ from time import sleep,time
 sys.path.append("..")
 import ax12 as ax12
 
-a = Ax12()
-if __name__=="__main__":
+a = ax12.Ax12()
+while(1):
 	a.move(11, 10)
 	time.sleep(2)
 	a.move(11, 30)
 	time.sleep(2)
+
 			
 
